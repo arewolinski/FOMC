1969ff71a7024bda38047fe1045b4e816e24481b
alexrewolinski <ajr12@rice.edu>
2023-09-28 14:11:23 -0500

    Done for now

--- main.rice/preprocessing.py
+++ main.rice/preprocessing.py
@@ -1,1 +1,30 @@
-print("test")
+#This is the file where all of the pre-processing for the transcripts will be done. Will add more files
+#and update structure as necessary. Refer to API for a more thorough explanation of what everything does
+
+
+def extractTextFromPdf(pdfFilePath):
+    """
+    Takes in a PDF File Path (which is the path to the transcripts released by the FOMC) and turns this into a text
+    file which can be digested by Python.
+
+    Inputs: pdfFilePath - A String representing the path to the PDF file
+
+    Outputs: textFile - A String representing the path to the txt file
+    """
+
+    pass
+
+def loadTranscripts(textFilePath):
+
+    """
+
+    """
+    pass
+
+
+
+
+
+
+
+
