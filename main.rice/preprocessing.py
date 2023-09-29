6b39e8793e4eab009a7fb003919142b2ba729152
alexrewolinski <ajr12@rice.edu>
2023-09-28 22:00:36 -0500

    Started working on process_txt

--- main.rice/preprocessing.py
+++ main.rice/preprocessing.py
@@ -20,16 +20,20 @@ def pdf_to_text(path_to_pdf, path_to_txt):
 
 
     try:
+        #Creates a PdfReader object (check the PyPDF2 library for more details)
         pdf_reader = PyPDF2.PdfReader(path_to_pdf)
         text = ""
         
+        #Reading from pdf_reader using the extract_text() method found within the PyPDF2 library
         for page in pdf_reader.pages:
             text += page.extract_text()
         
         with open(path_to_txt, 'w', encoding='utf-8') as txt_file:
             txt_file.write(text)
     
+        #Useful print statements for debugging
         print(f"PDF file '{path_to_pdf}' successfully converted to '{path_to_txt}'.")
+
     except Exception as e:
         print(f"An error occurred: {str(e)}")
 
@@ -38,19 +42,50 @@ def pdf_to_text(path_to_pdf, path_to_txt):
 #pdf_to_text('main.rice/files/FOMC_2000_Meeting_Transcript.pdf', 'main.rice/files/sample.txt')
 
 
-def load_transcripts(textFilePath):
-
+def process_txt(path_to_txt):
     """
     Takes in a txt file path and returns a list of Strings which represents the words after they have been split 
-    at spaces. Notes that this function does not get rid of the spaces; that is for a later function
+    at spaces. Note that this function does not get rid of the spaces; that is for a later function
 
     Inputs: 
 
+    text_file_path - A String representing a path to a txt file.
+
+    Returns: A list of Strings representing the words within the txt file after they have been split at spaces.
+
     """
-    pass
 
 
-def pdf_to_final(pdfFilePath):
+    try:
+        words_with_spaces = []
+        line_check = 0 #debugging purpose
+
+        # Open the text file for reading line by line
+        with open(path_to_txt, 'r') as file:
+            for line in file:
+
+                # Splits the line into a list of strings separated by spaces
+                words = line.split()
+
+                for word in words:
+                    line_check += 1 #debugging purpose
+                    words_with_spaces.append(word)
+                    print(f"Processed word: {word} & line: {line_check}") #debuggin purpose
+
+
+        return words_with_spaces
+    except FileNotFoundError:
+        print(f"File not found: {path_to_txt}")
+        return []
+
+# Example usage:
+#result = process_txt("main.rice/files/sample.txt")
+#print(result)
+
+
+
+
+def pdf_to_final(pdf_file_path):
 
     """
     Takes in a PDF file path and uses all of the above functions to turn this PDF file path into a completely pre-processed transcript.
